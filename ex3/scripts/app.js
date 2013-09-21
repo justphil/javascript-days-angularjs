d5a5ce56a356389b494a64ec71fef5bc7ae1a820
Philipp Tarasiewicz <philipp.tarasiewicz@googlemail.com>
2013-09-21 18:38:38 +0200

    refactor(ex3): move application components to separate files

---
+++ ex3/scripts/app.js
@@ -0,0 +1,7 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: pt
+ * Date: 21.09.13
+ * Time: 18:34
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ ex3/scripts/directives/colorPicker.js
@@ -0,0 +1,7 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: pt
+ * Date: 21.09.13
+ * Time: 18:33
+ * To change this template use File | Settings | File Templates.
+ */

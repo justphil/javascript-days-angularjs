c3dac137e2a9c370b4eb52472befd2652bf671b8
Peter Rossbach <peterrossbach@me.com>
2013-09-23 16:51:46 +0200

    Add filter examples

---
+++ ex4/scripts/controllers/dateCtrl.js
@@ -0,0 +1,12 @@
+"use strict";
+
+app.controller('dateCtrl', function ($scope, $timeout) {
+      $scope.now = 'Loading...';
+      var updateTime = function() {
+            $timeout(function() {
+                  $scope.now = new Date();
+                  updateTime();
+            }, 1000);
+	 };
+     updateTime();
+});

457efefdc600d0a195477072d37ac13ae84dbb91
Peter Rossbach <peterrossbach@me.com>
2013-09-25 10:25:02 +0200

    add simple resolve example

--- ex5/scripts/configs/route.js
+++ ex5/scripts/configs/route.js
@@ -10,7 +10,16 @@ function routeConfig($routeProvider) {
 	}).
 	when('/edit', {
 		controller: 'editCtrl',
-		templateUrl: 'edit.html'
+		templateUrl: 'edit.html',
+		resolve: {
+			app: function ($q,$timeout) {
+				var defer = $q.defer();
+				$timeout(function() {
+					defer.resolve();
+				},2000);
+				return defer.promise;
+			}
+		}
 	}).
 	otherwise({
 		redirectTo: '/'

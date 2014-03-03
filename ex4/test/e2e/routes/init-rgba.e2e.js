bb89ae820a3a08256eb3aec76ef4d9099a0c03cc
Philipp Tarasiewicz <philipp.tarasiewicz@googlemail.com>
2014-03-03 12:44:37 +0000

    optimize(ex4): some improvements in the e2e tests

--- ex4/test/e2e/routes/init-rgba.e2e.js
+++ ex4/test/e2e/routes/init-rgba.e2e.js
@@ -2,12 +2,9 @@
 
 describe('E2E: init-rgba route', function() {
 
-    beforeEach(function() {
-        browser().navigateTo('/#/10/20/30/1.0');
-        browser().reload();
-    });
-
     it('should properly process the url path parameters', function() {
+        browser().navigateTo('/#/10/20/30/1.0');
+
         expect(element('div.preview').css('background-color')).toEqual('rgb(10, 20, 30)');
         expect(element('ul li:nth-child(1)').text()).toEqual('R: 10');
         expect(element('ul li:nth-child(2)').text()).toEqual('G: 20');
